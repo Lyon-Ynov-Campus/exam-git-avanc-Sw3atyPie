be4634ae6da94d4fd7fda8ed311d502f3c24b304
ANome1 <anomenafitia@gmail.com>
2025-10-27 15:02:30 +0100

    rebase feature url

--- main.go
+++ main.go
@@ -6,23 +6,17 @@ import (
 	"net/http"
 )
 
-<<<<<<< HEAD
 func URLHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>URL visitée</h1><p>Vous avez visité : %s</p>", r.URL.Path)
 }
 
-=======
->>>>>>> 0168ec7 (Initialize dev)
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<h1>Site Web en Go</h1><p>Page d'accueil</p>")
 	})
 
-<<<<<<< HEAD
 	http.HandleFunc("/url", URLHandler)
 
-=======
->>>>>>> 0168ec7 (Initialize dev)
 	fmt.Println("Serveur démarré sur http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
