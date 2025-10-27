504957341a05488f457ef1c30e0f6c95fe2a7413
ANome1 <anomenafitia@gmail.com>
2025-10-27 15:27:47 +0100

    m

--- script/fake-colleague.sh
+++ script/fake-colleague.sh
@@ -1,2 +1,8 @@
-chmod +x ./script
-./fake-colleague.sh https://github.com/Lyon-Ynov-Campus/exam-git-avanc-Sw3atyPie
+#!/usr/bin/env bash
+
+git clone $1 /tmp/git_exam
+cd /tmp/git_exam
+git checkout feature-audio-support
+echo "func tacos(){}" >> main.go
+git add . && git commit -m "add : func tacos (to dev)" && git push
+rm -rf /tmp/git_exam
